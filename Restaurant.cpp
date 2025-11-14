d3bdd0955ec93f29f0064aca3c112cb4593d2552
RemusCocias <remi.cocias@gmail.com>
2025-11-14 16:22:58 +0200

    Add project files.

---
+++ Restaurant.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// ====== CLASA RESTAURANT ======
+class Restaurant {
+public:
+    // aici vor veni atributele, constructorii etc. in commit-urile urmatoare
+};
+
+// ====== CLASA ANGAJAT ======
+class Angajat {
+public:
+    // aici vor veni atributele, constructorii etc.
+};
+
+// ====== CLASA CLIENT ======
+class Client {
+public:
+    // aici vor veni atributele, constructorii etc.
+};
+
+int main() {
+    cout << "Proiect OOP - structura initiala creata." << endl;
+    return 0;
+}
